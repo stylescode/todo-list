7da17d2c124d20448f8fbb31bd9a2bc2b5c3743a
Styles <stylesk11@gmail.com>
2024-05-09 10:59:55 -0700

    create layout for dashboard elements

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const DashLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
+
+export default DashLayout
