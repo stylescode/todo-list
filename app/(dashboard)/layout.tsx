2dfdc891a67313edb5df21349b4345bde6876ec2
Styles <stylesk11@gmail.com>
2024-05-13 13:35:17 -0700

    layout setup

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,8 +1,10 @@
+import NewTodoForm from "@/components/NewTodoForm"
 import React from "react"
 
 const DashLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
+      <NewTodoForm />
       {children}
     </div>
   )
